4a631d7c91819565b7178dd6224fd8e24c457924
lmo125 <151047795+lmo125@users.noreply.github.com>
2023-11-28 11:52:32 +0100

    Add files via upload

---
+++ redirection.php
@@ -0,0 +1,5 @@
+<?php
+  header('Status: 301 Moved Permanently', false, 301);      
+  header('Location: https://aoprestlse.maps.arcgis.com/apps/webappviewer/index.html?id=670a69db9e764647b0c0f11ba20bd2ac ');      
+  exit();
+?>
